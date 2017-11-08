2531310a9517ee44f81f21359c356176b5c31a14
Rafael Santos <raf.rsr@gmail.com>
2017-11-08 09:11:12 -0500

    update tests

---
+++ tests/Swagger/Specification/Root/InfoSpecTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Swagger\Specification\Root;
+
+use Ynlo\RestfulPlatformBundle\Swagger\Model\SwaggerObject;
+use Ynlo\RestfulPlatformBundle\Swagger\Specification\Root\InfoSpec;
+use PHPUnit\Framework\TestCase;
+
+class InfoSpecTest extends TestCase
+{
+    public function testDecorator()
+    {
+        $info = (new InfoSpec('Some API', 'Description', 'v1.0'))->getDecorator();
+        $swObject = new SwaggerObject();
+        $info($swObject);
+
+        self::assertEquals('Some API', $swObject->getInfo()->getName());
+        self::assertEquals('Description', $swObject->getInfo()->getDescription());
+        self::assertEquals('v1.0', $swObject->getInfo()->getVersion());
+    }
+}
