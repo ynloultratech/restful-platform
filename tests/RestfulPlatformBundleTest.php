ed83af07c26fe8d49eb2417565b557cff7ba3463
Rafael Santos <raf.rsr@gmail.com>
2017-11-09 08:13:31 -0500

    update tests

--- tests/RestfulPlatformBundleTest.php
+++ tests/RestfulPlatformBundleTest.php
@@ -4,7 +4,10 @@ namespace Tests;
 
 use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Ynlo\RestfulPlatformBundle\Api\RestApiInterface;
 use Ynlo\RestfulPlatformBundle\RestfulPlatformBundle;
 
 class RestfulPlatformBundleTest extends TestCase
@@ -21,5 +24,18 @@ class RestfulPlatformBundleTest extends TestCase
 
         $bundle->setContainer($containerMock);
         $bundle->boot();
+    }
+
+    public function testBuild()
+    {
+        /** @var ContainerBuilder|\PHPUnit_Framework_MockObject_MockObject $container */
+        $container = self::createMock(ContainerBuilder::class);
+
+        $childDefinition = self::createMock(ChildDefinition::class);
+        $childDefinition->expects(self::once())->method('addTag')->with('restful_platform.rest_api');
+        $container->expects(self::once())->method('registerForAutoconfiguration')->with(RestApiInterface::class)->willReturn($childDefinition);
+
+        $bundle = new RestfulPlatformBundle();
+        $bundle->build($container);
     }
 }
