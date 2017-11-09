a56726d70a53de7d24e633de1ff9f78445434cb4
Rafael Santos <raf.rsr@gmail.com>
2017-11-09 08:30:41 -0500

    update tests

--- tests/Fixtures/Model/User.php
+++ tests/Fixtures/Model/User.php
@@ -4,11 +4,15 @@ namespace Tests\Fixtures\Model;
 
 use JMS\Serializer\Annotation as Serializer;
 
+/**
+ * @Serializer\ExclusionPolicy("all")
+ */
 class User
 {
     /**
      * @var string
      *
+     * @Serializer\Expose()
      * @Serializer\SerializedName("username")
      * @Serializer\Type("string")
      */
@@ -17,6 +21,7 @@ class User
     /**
      * @var string
      *
+     * @Serializer\Expose()
      * @Serializer\SerializedName("first_name")
      * @Serializer\Type("string")
      *
@@ -27,10 +32,84 @@ class User
     /**
      * @var string
      *
+     * @Serializer\Expose()
      * @Serializer\SerializedName("last_name")
      * @Serializer\Type("string")
      *
      * @Serializer\Groups({"public"})
      */
     protected $lastName;
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername(string $username): User
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName(string $firstName): User
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName(string $lastName): User
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\Expose()
+     * @Serializer\VirtualProperty("fullName")
+     * @Serializer\SerializedName("fullName")
+     * @Serializer\Type("string")
+     *
+     * @return string
+     */
+    public function getFullName(): string
+    {
+        return $this->firstName.' '.$this->lastName;
+    }
 }
