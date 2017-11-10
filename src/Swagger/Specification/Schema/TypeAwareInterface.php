f6f10a94d3eb1af43fa733b328d1b7433af87637
Rafael Santos <raf.rsr@gmail.com>
2017-11-10 08:29:10 -0500

    Add type and format constants in TypeAwareInterface

--- src/Swagger/Specification/Schema/TypeAwareInterface.php
+++ src/Swagger/Specification/Schema/TypeAwareInterface.php
@@ -13,6 +13,22 @@ namespace Ynlo\RestfulPlatformBundle\Swagger\Specification\Schema;
 
 interface TypeAwareInterface
 {
+    const TYPE_STRING = 'string';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_NUMBER = 'number';
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_ARRAY = 'array';
+
+    const FORMAT_INT32 = 'int32';
+    const FORMAT_INT64 = 'int64';
+    const FORMAT_FLOAT = 'float';
+    const FORMAT_DOUBLE = 'double';
+    const FORMAT_BYTE = 'byte'; //base64 encoded characters
+    const FORMAT_BINARY = 'binary'; //any sequence of octets
+    const FORMAT_DATE = 'date';  //As defined by full-date - RFC3339
+    const FORMAT_DATETIME = 'date-time';  //As defined by full-date - RFC3339
+    const FORMAT_PASSWORD = 'password';  //Used to hint UIs the input needs to be obscured.
+
     /**
      * @return string
      */
