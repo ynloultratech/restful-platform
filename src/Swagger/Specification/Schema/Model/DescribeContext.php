65f0da7e7ba1924229e68687af166f98167808ee
Rafael Santos <raf.rsr@gmail.com>
2017-11-13 20:47:40 -0500

    Update project files copyright header
    - Delete .licenser.yml file, using integrated IDE tool (PHPStorm/Copyright)

--- src/Swagger/Specification/Schema/Model/DescribeContext.php
+++ src/Swagger/Specification/Schema/Model/DescribeContext.php
@@ -1,12 +1,11 @@
 <?php
-/*
+/**
  * LICENSE: This file is subject to the terms and conditions defined in
  * file 'LICENSE', which is part of this source code package.
  *
  * @copyright 2017 Copyright(c) - All rights reserved.
- *
- * @author YNLO-Ultratech Development Team <developer@ynloultratech.com>
- * @package restful-platform
+ * @author    YNLO-Ultratech Development Team <developer@ynloultratech.com>
+ * @package   restful-platform
  */
 
 namespace Ynlo\RestfulPlatformBundle\Swagger\Specification\Schema\Model;
--- src/Tests/Routing/HateoasRouteGeneratorTest.php
+++ src/Tests/Routing/HateoasRouteGeneratorTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2017 Copyright(c) - All rights reserved.
+ * @author    YNLO-Ultratech Development Team <developer@ynloultratech.com>
+ * @package   restful-platform
+ */
 
 namespace Ynlo\RestfulPlatformBundle\Tests\Routing;
 
