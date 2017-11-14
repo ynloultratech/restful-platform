d89dc08ceffaa350c5d42bf79ec75a3b16c561d3
Rafael Santos <raf.rsr@gmail.com>
2017-11-13 20:16:59 -0500

    Add ApiTestCase as helper to create functional tests

---
+++ src/Test/ApiTestCase.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2017 Copyright(c) - All rights reserved.
+ *
+ * @author YNLO-Ultratech Development Team <developer@ynloultratech.com>
+ * @package restful-platform
+ */
+
+namespace Ynlo\RestfulPlatformBundle\Test;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Client;
+
+class ApiTestCase extends WebTestCase
+{
+    private static $client;
+
+    /**
+     * @inheritdoc
+     */
+    protected static function createClient(array $options = [], array $server = [])
+    {
+        $client = parent::createClient($options, $server);
+        self::$client = $client;
+
+        return $client;
+    }
+
+    /**
+     * @return Client
+     */
+    protected static function getClient(): Client
+    {
+        return self::$client ?? self::createClient();
+    }
+
+    /**
+     * @param string $path
+     * @param array  $parameters
+     */
+    protected static function sendGET($path, array $parameters = [])
+    {
+        self::getClient()->request(Request::METHOD_GET, $path, $parameters);
+    }
+
+    /**
+     * Check if the latest response is a valid JSON
+     */
+    protected static function assertResponseIsValidJson()
+    {
+        $response = self::getClient()->getResponse();
+
+        self::assertNotNull($response);
+        self::assertEquals('application/json', $response->headers->get('Content-Type'));
+        self::assertJson($response->getContent());
+    }
+}
